b661dd175ec1854c8af4c254f139dcb38543e9ab
cprg352 <cprg352@demongeek.com>
2020-09-21 12:49:03 -0600

    Added validation Added validation to ensure the user enters both first and last name.

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -28,9 +28,14 @@ public class HelloWorldServlet extends HttpServlet {
             throws ServletException, IOException {
         String firstName = request.getParameter("firstname");
         String lastName = request.getParameter("lastname");
-        
+                   
         request.setAttribute("firstname", firstName);
         request.setAttribute("lastname", lastName);
+        
+        if (firstName == null || firstName.equals("") || lastName == null || lastName.equals("")) {
+            getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp").forward(request, response);
+            return;
+        }
         
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp").forward(request, response);
     }
